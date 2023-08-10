40e1828a693f3d67b3e6735e46d862aea5002ad4
Romulo Rosa @romulorosadev <romulorosadev@gmail.com>
2023-08-09 23:10:01 -0300

    inserção template string dados cada livro

--- src/app/main.js
+++ src/app/main.js
@@ -8,7 +8,8 @@
 // 8) Declarar var para atribuir o elemento que receberá todos os livros
 // 9)  Declarar a função pra exibir os livros passando por parametro uma lista de livros
 // 10) O array passado por parâmetro é iterado usando o método forReach, assim realiza a mesma função do primeiro ao último item dessa lista/array
-// 11) Para o momento exibimos os dados referente a cada item iterado
+// 11) A cada iteração, é adicionado um elemento HTML
+// 12) O elemeto HTML é um template string que traz os atributos de cada item iterado dessa lista/array
 
 
 //9
@@ -16,7 +17,19 @@ function exibirLivros(livrosLista){
     //10
     livrosLista.forEach (livrosItem => {
         //11
-        console.log(livrosItem)
+        outLivros.innerHTML += 
+        //12
+        `<div class="livro">
+            <img class="livro__imagens" src=${livrosItem.imagem} alt=${livrosItem.alt} />
+            <h2 class="livro__titulo">
+                ${livrosItem.titulo}
+            </h2>
+            <p class="livro__descricao">${livrosItem.autor}</p>
+            <p class="livro__preco" id="preco">${livrosItem.preco.toFixed(2)}</p>
+            <div class="tags">
+                <span class="tag">${livrosItem.categoria}</span>
+            </div>
+        </div>`;        
     });    
 }
 
