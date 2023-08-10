a18625fb0f2aec326a918e850bce1887670a86f3
Romulo Rosa @romulorosadev <romulorosadev@gmail.com>
2023-08-09 22:46:12 -0300

    js function exibirLivros

--- src/app/main.js
+++ src/app/main.js
@@ -4,7 +4,21 @@
 // 4) Criar uma função assincrona
 // 5) Ao chamar essa função sua resposta deve esperar a requisição ao endpoint
 // 6) A resposta deve esperar montar o json e ao final atribuir a livros
-// 7) Iteramos o array de livros com um método especial por ser muito menos verboso e manter mais organização no código. Esse metodo realiza a mesma função pra cada item do array
+// 7) Chamar a função pra exibir os livros
+// 8) Declarar var para atribuir o elemento que receberá todos os livros
+// 9)  Declarar a função pra exibir os livros passando por parametro uma lista de livros
+// 10) O array passado por parâmetro é iterado usando o método forReach, assim realiza a mesma função do primeiro ao último item dessa lista/array
+// 11) Para o momento exibimos os dados referente a cada item iterado
+
+
+//9
+function exibirLivros(livrosLista){
+    //10
+    livrosLista.forEach (livrosItem => {
+        //11
+        console.log(livrosItem)
+    });    
+}
 
 
 //4
@@ -13,10 +27,8 @@ async function getBuscarLivrosApi() {
     const resposta = await fetch (endPointApi);
     //6
     livros = await resposta.json();
-    //7
-    livros.forEach (livro => {
-        console.log(livro)
-    });
+    //7    
+    exibirLivros(livros);    
 }
 
 
@@ -26,3 +38,5 @@ let livros = [];
 const endPointApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 //3
 getBuscarLivrosApi();
+//8
+const outLivros = document.getElementById("livros");
